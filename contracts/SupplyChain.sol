fd794163e7088fd8666a6c6cf13f048878fcc6f2
Joonbo Rhie <jb.rhie@gmail.com>
2020-01-04 20:09:36 -0500

    Added events and structs

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -12,10 +12,12 @@ contract SupplyChain {
   address owner;
 
   /* Add a variable called skuCount to track the most recent sku # */
+  uint private skuCouint;
 
   /* Add a line that creates a public mapping that maps the SKU (a number) to an Item.
      Call this mappings items
   */
+  mapping (uint => Item) public items;
 
   /* Add a line that creates an enum called State. This should have 4 states
     ForSale
@@ -24,17 +26,30 @@ contract SupplyChain {
     Received
     (declaring them in this order is important for testing)
   */
-
+  enum State { ForSale, Sold, Shipped, Received }
+     
   /* Create a struct named Item.
     Here, add a name, sku, price, state, seller, and buyer
     We've left you to figure out what the appropriate types are,
     if you need help you can ask around :)
     Be sure to add "payable" to addresses that will be handling value transfer
   */
+  struct Item {
+    bytes32 name;
+    uint sku;
+    uint price;
+    State state;
+    address payable seller;
+    address payable buyer;
+  }
 
   /* Create 4 events with the same name as each possible State (see above)
     Prefix each event with "Log" for clarity, so the forSale event will be called "LogForSale"
     Each event should accept one argument, the sku */
+  event LogForSale(uint sku);
+  event LogSold(uint sku);
+  event LogShipped(uint sku);
+  event LogReceived(uint sku);  
 
 /* Create a modifer that checks if the msg.sender is the owner of the contract */
 
@@ -56,7 +71,7 @@ contract SupplyChain {
    so checking that Item.State == ForSale is not sufficient to check that an Item is for sale.
    Hint: What item properties will be non-zero when an Item has been added?
    */
-  modifier forSale
+  modifier forSale(uint _sku, )
   modifier sold
   modifier shipped
   modifier received
